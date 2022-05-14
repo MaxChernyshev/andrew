0db1c50ca34462f686400cf65dcb950062f8033d
chernyshevk <ivanka.chernysheva@gmail.com>
2022-05-14 13:35:36 -0700

    first front commit

---
+++ resources/views/front/faq.blade.php
@@ -0,0 +1,74 @@
+@extends('front.layouts.layout')
+
+@section('content')
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <section>
+                    <h2 class="title_FAQ">FAQ</h2>
+
+                    <div class="faq">
+                        <div class="question">
+                            <h3>what is blabla</h3>
+
+                            <svg width="15" height="10" viewBox="0 0 42 25">
+                                <path
+                                    d="M3 3L21 21L39 3"
+                                    stroke="white"
+                                    stroke-width="7"
+                                    stroke-linecap="round"
+                                />
+                            </svg>
+                        </div>
+
+                        <div class="answer">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores corporis cum cupiditate debitis dolorum eius exercitationem expedita iusto molestiae necessitatibus omnis pariatur quae qui quibusdam quidem, sed soluta tempora vitae.</p>
+                        </div>
+                    </div>
+
+                    <div class="faq">
+                        <div class="question">
+                            <h3>what is blabla</h3>
+
+                            <svg width="15" height="10" viewBox="0 0 42 25">
+                                <path
+                                    d="M3 3L21 21L39 3"
+                                    stroke="white"
+                                    stroke-width="7"
+                                    stroke-linecap="round"
+                                />
+                            </svg>
+                        </div>
+
+                        <div class="answer">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores corporis cum cupiditate debitis dolorum eius exercitationem expedita iusto molestiae necessitatibus omnis pariatur quae qui quibusdam quidem, sed soluta tempora vitae.</p>
+                        </div>
+                    </div>
+
+                    <div class="faq">
+                        <div class="question">
+                            <h3>what is blabla</h3>
+
+                            <svg width="15" height="10" viewBox="0 0 42 25">
+                                <path
+                                    d="M3 3L21 21L39 3"
+                                    stroke="white"
+                                    stroke-width="7"
+                                    stroke-linecap="round"
+                                />
+                            </svg>
+                        </div>
+
+                        <div class="answer">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores corporis cum cupiditate debitis dolorum eius exercitationem expedita iusto molestiae necessitatibus omnis pariatur quae qui quibusdam quidem, sed soluta tempora vitae.</p>
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+        </div>
+    </div>
+
+@endsection
