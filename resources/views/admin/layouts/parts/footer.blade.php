07b0e4430ea8f5757570315ea32f57122603bd5c
MaxChernyshev <maksim.chernyshev@gmail.com>
2022-06-03 18:54:05 +0300

    admin pass

--- resources/views/admin/layouts/parts/footer.blade.php
+++ resources/views/admin/layouts/parts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer fixed-bottom mt-0 pt-0 mb-0 pb-0">
-    <small><strong>Copyright © {{ now()->year }} <a href="https://maksimchernyshev.com" target="_blank">Max Chernyshev</a>.</strong> All rights reserved.</small>
+    <small><strong>Powered by Laravel {{ now()->year }} <a href="https://github.com/MaxChernyshev" target="_blank">Max Chernyshev</a></strong> </small>
 </footer>
 <script src="{{ asset('js/admin/js/adminlte.js') }}"></script>
 @if(in_array(Route::current()->getName(),['admin.subjects.create', 'admin.subjects.edit', 'admin.questions.create', 'admin.questions.edit']))
