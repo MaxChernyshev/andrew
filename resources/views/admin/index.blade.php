f990b0be7b3a744ff52ffcdfad6787042302f124
MaxChernyshev <maksim.chernyshev@gmail.com>
2022-06-05 11:17:58 +0300

    small fixes in admin blades

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,13 @@
 @extends('admin.layouts.layout')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="mt-5 text-black">
-                    <h1>Welcome to the admin panel <strong class="app-name">{{ env('APP_NAME') }}</strong></h1>
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="mt-5 text-black">
+                        <h1>Welcome to the admin panel <strong class="app-name">{{ env('APP_NAME') }}</strong></h1>
+                    </div>
                 </div>
             </div>
         </div>
